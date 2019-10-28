6c466721cbb6ae7703300bbe934354fe46d767e9
Leina33 <dominickrutto@gmail.com>
2019-10-28 14:45:32 +0300

    This is an eventemitter method that drive the clickeable events in complete button

--- src/app/qoutes-detail/qoutes-detail.component.ts
+++ src/app/qoutes-detail/qoutes-detail.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input} from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 import { Qoutes } from '../qoutes';
+
 
 @Component({
   selector: 'app-qoutes-detail',
@@ -7,6 +8,12 @@ import { Qoutes } from '../qoutes';
   styleUrls: ['./qoutes-detail.component.css']
 })
 export class QoutesDetailComponent implements OnInit {
+  @Output() isComplete = new EventEmitter<boolean>();
+
+
+  quoteComplete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
 
   @Input() quote:Qoutes;
 
